8491f042c89034fc8a0fc9b13fc230b9fb579011
DTRaptor <58063566+DTRaptor@users.noreply.github.com>
2019-11-22 21:55:35 +1100

    Add files via upload

---
+++ main/java/com/dtraptor/coolmod/client/ModelRegistrationHandler.java
@@ -0,0 +1,28 @@
+package com.dtraptor.coolmod.client;
+
+//fresh imports
+import com.dtraptor.coolmod.CoolMod;
+import com.dtraptor.coolmod.init.ModItems;
+
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.event.ModelRegistryEvent;
+import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+@EventBusSubscriber(value = Side.CLIENT, modid = CoolMod.mod_id)
+public class ModelRegistrationHandler {
+
+	@SubscribeEvent
+	public static void registerModels(ModelRegistryEvent event) {
+		registerModel(ModItems.FIRST_ITEM, 0);
+	}
+
+	private static void registerModel(Item item, int meta) {
+		ModelLoader.setCustomModelResourceLocation(item, meta, 
+				new ModelResourceLocation(item.getRegistryName(), "inventory"));
+	}
+
+}
